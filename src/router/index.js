c5f7dd0867b672676280f789b599b3340274273d
liuzekai6666 <zliu0307@student.monash.edu>
2025-08-14 23:53:07 +0800

    trigger deploy

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,8 @@
-// router/index.js
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import WeatherView from '../views/WeatherView.vue'
 import CountBookAPI from '../views/CountBookAPI.vue'
 
 const routes = [
-  {
-    path: '/',
-    redirect: '/weather'  // 根路径自动重定向到 /weather
-  },
   {
     path: '/weather',
     name: 'GetWeather',
@@ -21,7 +16,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory('/12/'), // hash 模式，保证 GitHub Pages 不 404
+  history: createWebHistory(),
   routes
 })
 
