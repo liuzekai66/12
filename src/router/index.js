9ca22deef009cb38b36f6e8af9bc3c58e7770313
liuzekai6666 <zliu0307@student.monash.edu>
2025-08-15 00:09:19 +0800

    fix paths for GitHub Pages

--- src/router/index.js
+++ src/router/index.js
@@ -3,21 +3,12 @@ import WeatherView from '../views/WeatherView.vue'
 import CountBookAPI from '../views/CountBookAPI.vue'
 
 const routes = [
-  {
-    path: '/weather',
-    name: 'GetWeather',
-    component: WeatherView
-  },
-  {
-    path: '/CountBookAPI',
-    name: 'CountBookAPI',
-    component: CountBookAPI
-  }
+  { path: '/weather', name: 'GetWeather', component: WeatherView },
+  { path: '/CountBookAPI', name: 'CountBookAPI', component: CountBookAPI }
 ]
 
 const router = createRouter({
-  // ⚠ 这里加上你的仓库名作为 base，注意前后斜杠
-  history: createWebHistory('/12/'),  
+  history: createWebHistory('/12/'),  // ⚠ 与 base 保持一致
   routes
 })
 
